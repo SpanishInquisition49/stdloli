179fa856b2f98d3c48303ed9f9fdc2cd540e76af
Gabriele Scannagatti <71408527+SpanishInquisition49@users.noreply.github.com>
2022-09-30 14:01:02 +0200

    Added the header file
    Co-authored-by: Barbasilico <91752284+Barbasilico@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # stdloli
-Turn C code into your favourite hentai
+
+This is a meme header file that define a macro for most of the basic C syntax with a "Manga" version of it
+
+## Example
+````c
+#include<stdio.h>
+ore_wa_ochin_chin_go_daisuki_nanda_yo(void){
+    ahegao cock[] = "Hentai\n" sugoi
+    look_at_my("%s", cock) sugoi
+    yamete kudasai sugoi
+}
+// It's equivalent to
+
+int main(void){
+    char cock[] = "Hentai\n";
+    printf("%s", cock);
+    return 0;
+}
+````
+
+Inspired by [Senpai](https://esolangs.org/wiki/Senpai) eso lang. ❤️
---
+++ stdloli.h
@@ -0,0 +1,24 @@
+#define onichan int
+#define ahegao char
+#define oppai double
+#define futanari float
+#define it_so_big long
+#define ara *
+#define uwu &
+#define sugoi ;
+#define yamete return
+#define chotto return
+#define kudasai 0
+#define made 1
+#define if_you_likey(expr) if(expr)
+#define otherwise else
+#define show_me_your(...) scanf(__VA_ARGS__)
+#define look_at_my(...) printf(__VA_ARGS__)
+#define we_can_stop_now break
+#define ore_wa_ochin_chin_go_daisuki_nanda_yo(...) int main(__VA_ARGS__)
+#define let_s_keep_this_going_as_long_as_you_likey(expr) while(expr)
+#define let_s_keep_this_going_for(a, e, e2) for(a;e;e2)
+#define can_i_see_your(...) malloc(__VA_ARGS__)
+#define get_rid_of(...) free(__VA_ARGS__)
+
+
